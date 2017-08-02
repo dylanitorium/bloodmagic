72a1b773cb817623f8972b508c3135e4662b320f
Dylan Sweetensen <dylan.sweetensen@education.govt.nz>
2017-08-03 10:04:44 +1200

    Execute and stream from remote server

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require('express');
 const http = require('http');
 const fs = require('fs');
 const spawn = require('child_process').spawn;
-const zlib = require('zlib');
+const createGzip = require('zlib').createGzip;
+const through = require('stream').PassThrough;
+const rexec = require('remote-exec');
+const path = require('path');
 
 const PORT = process.env.PORT || '8866';
 const COMMAND = process.env.COMMAND;
+
+const stream = new through();
+
+const options = {
+  port: 22,
+  username: 'centos',
+  privateKey: fs.readFileSync('/Users/deadsalt/.ssh/nzccp01.pem'),
+  stdout: stream,
+};
+
+const host = [ 'nzccp.co.nz' ];
+
+const command = [ COMMAND ];
 
 const executable = (cmd) => {
   return cmd.split(' ').shift();
@@ -16,18 +32,22 @@ const params = (cmd) => {
 };
 
 const gzip = () => {
-  return new zlib.createGzip();
+  return new createGzip();
 };
 
 const service = express();
+
 service.get('*', (request, response) => {
   response.attachment('export.sql.gz');
-  spawn(executable(COMMAND), params(COMMAND))
-    .stdout
-    .pipe(gzip())
-    .pipe(response)
-    .on('error', console.error);
+  rexec(host, command, options, (error) => {
+    if (error) {
+      console.log(error);
+    }
+    response.end();
+  });
+  stream.pipe(gzip()).pipe(response).on('error', console.error);
 });
+
 service.listen(PORT);
 
 
