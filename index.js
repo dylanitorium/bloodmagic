8598d008e552635640fcaaec81d93097317ccc1b
Dylan Sweetensen <dylan.sweetensen@education.govt.nz>
2017-08-03 09:37:07 +1200

    Pipe output straight to response

--- index.js
+++ index.js
@@ -2,49 +2,33 @@ const express = require('express');
 const http = require('http');
 const fs = require('fs');
 const spawn = require('child_process').spawn;
+const zlib = require('zlib');
 
-const EXPORT_NAME = '/Users/deadsalt/export.tar.gz';
 const PORT = process.env.PORT || '8866';
+const COMMAND = process.env.COMMAND;
+
+const executable = (cmd) => {
+  return cmd.split(' ').shift();
+};
+
+const params = (cmd) => {
+  return cmd.split(' ').splice(1);
+};
+
+const gzip = () => {
+  return new zlib.createGzip();
+};
 
 const service = express();
-
-const createExport = (response) => {
-  return new Promise((resolve, reject) => {
-    const createExport = spawn('sh', [
-      '/Users/deadsalt/Projects/nzccp/public_html/_scripts/export-db-docker.sh',
-      '/Users/deadsalt/export.sql'
-    ]);
-    createExport.on('close', () => {
-      resolve();
-    })
-  });
-};
-
-const createTar = (response) => {
-  return new Promise((resolve, reject) => {
-    const createExport = spawn('tar', [
-      '-cvzf',
-      EXPORT_NAME,
-      '/Users/deadsalt/export.sql'
-    ]);
-    createExport.on('close', () => {
-      resolve();
-    })
-  });
-};
-
-const sendFile = (response) => {
-  return new Promise((reject, resolve) => {
-    response.download(EXPORT_NAME, 'export.tar.gz');
-    return resolve();
-  });
-};
-
 service.get('*', (request, response) => {
-  createExport(response)
-    .then(() => createTar(response))
-    .then(() => sendFile(response))
-    .catch(console.error);
+  response.attachment('export.sql.gz');
+  spawn(executable(COMMAND), params(COMMAND))
+    .stdout
+    .pipe(gzip())
+    .pipe(response)
+    .on('error', console.error);
 });
-
 service.listen(PORT);
+
+
+
